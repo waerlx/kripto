760882179937d8a9c925344c6636befe4b12e56f
Adelina <adelinaakylbekova565@gmail.com>
2024-04-14 00:06:38 +0600

    del obj data

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -5,15 +5,7 @@ import { fakeFetchCrypto, fetchAssets } from '../../api';
 import { percentDifference } from '../../utils'
 const siderStyle = {
     padding: '1rem',
-};
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
-
+}
 
 export default function AppSider() {
 
